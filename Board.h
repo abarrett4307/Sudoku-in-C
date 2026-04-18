0ae5d9c00af54b3a5eb1bb0425c704e413611653
justinli7600 <justinli7600@gmail.com>
2026-04-17 20:43:47 -0700

    Create Board.h

---
+++ Board.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+void initializeBoard(int **board, int rows, int cols);
+void printBoard(int **board, int rows, int cols);
+void freeBoard(int **board, int rows);
+
+#endif

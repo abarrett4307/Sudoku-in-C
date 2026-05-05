8315e95859fed3dfba95f7e7f977148a1e54831b
justinli7600 <justinli7600@gmail.com>
2026-05-04 18:39:17 -0700

    Board.c
    Updated file

--- Board.c
+++ Board.c
@@ -2,85 +2,118 @@
 #include <stdlib.h>
 #include "Board.h"
 
-void initializeBoard(int **board, int rows, int cols)
-{
-    int i, j;
+int **createBoard(void) {
+    int **board = malloc(SIZE * sizeof(int *));
 
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j < cols; j++)
-        {
-            board[i][j] = 0;
+    if (board == NULL) {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        board[i] = malloc(SIZE * sizeof(int));
+
+        if (board[i] == NULL) {
+            printf("Memory allocation failed.\n");
+
+            for (int j = 0; j < i; j++) {
+                free(board[j]);
+            }
+
+            free(board);
+            return NULL;
         }
     }
+
+    return board;
 }
 
-void printBoard(int **board, int rows, int cols)
-{
-    int i, j;
-
-    printf("\n");
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j < cols; j++)
-        {
-            if (board[i][j] == 0)
-            {
-                printf(". ");
-            }
-            else
-            {
-                printf("%d ", board[i][j]);
-            }
-        }
-        printf("\n");
+void freeBoard(int **board) {
+    if (board == NULL) {
+        return;
     }
-}
 
-void freeBoard(int **board, int rows)
-{
-    int i;
-
-    for (i = 0; i < rows; i++)
-    {
+    for (int i = 0; i < SIZE; i++) {
         free(board[i]);
     }
+
     free(board);
 }
 
-int isValidMove(int **board, int row, int cols, int num)
-{
-    int i, j;
-    int startRow, startCols;
+void copyBoard(int **dest, int **src) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            dest[i][j] = src[i][j];
+        }
+    }
+}
 
-//checking row
-    for (i = 0; i < 9; i++)
-    {
-        if (board[row][i] == num)
-        {
+void printBoard(int **board) {
+    printf("\n");
+    printf("    0 1 2   3 4 5   6 7 8\n");
+    printf("  +-------+-------+-------+\n");
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d | ", i);
+
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == 0) {
+                printf(". ");
+            } else {
+                printf("%d ", board[i][j]);
+            }
+
+            if ((j + 1) % 3 == 0) {
+                printf("| ");
+            }
+        }
+
+        printf("\n");
+
+        if ((i + 1) % 3 == 0) {
+            printf("  +-------+-------+-------+\n");
+        }
+    }
+}
+
+int isBoardFull(int **board) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == 0) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+int isValidMove(int **board, int row, int col, int value) {
+    int boxRow;
+    int boxCol;
+
+    // Check row
+    for (int j = 0; j < SIZE; j++) {
+        if (board[row][j] == value) {
             return 0;
         }
     }
 
-// checking column
-    for (i = 0; i < 9; i++)
-    {
-        if (board[i][cols] == num)
-        {
+    // Check column
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][col] == value) {
             return 0;
         }
     }
 
-    startRow = (row / 3) * 3;
-    startCols = (cols / 3) * 3;
+    // Find top-left corner of the 3x3 box
+    boxRow = row - row % 3;
+    boxCol = col - col % 3;
 
-// checking the 3 x 3 boxes
-    for (i = startRow; i < startRow + 3; i++)
-    {
-        for (j = startCols; j < startCols + 3; j++)
-        {
-            if (board[i][j] == num)
-            {
+    // Check 3x3 box
+    for (int i = boxRow; i < boxRow + 3; i++) {
+        for (int j = boxCol; j < boxCol + 3; j++) {
+            if (board[i][j] == value) {
                 return 0;
             }
         }
